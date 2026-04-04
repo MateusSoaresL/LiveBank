198587b1d22e1446ba4fb992e20888f58a358cff
Mateus Soares Lima Santos <mateussoareslimasantos2013@gmail.com>
2026-04-04 17:03:44 -0300

    LiveBank, i added 3 functions, add money, to transfer money and Exit.

---
+++ Account.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Account {
+    private double Balance = 0;
+    private double Value;
+
+    public void Balance() {
+        if (Balance == 0) {
+            System.out.println(Utilities.ANSI.BOLD + "Balance: " + Balance + Utilities.ANSI.RESET);
+        } else if (Balance > 0) {
+            System.out.println(Utilities.ANSI.BOLD_GREEN + "Balance: " + Balance + Utilities.ANSI.RESET);
+        } else if (Balance < 0) {
+            System.out.println(Utilities.ANSI.BOLD_RED + "Balance: " + Balance + Utilities.ANSI.RESET);
+        } else {
+            System.out.println(Utilities.ANSI.BOLD_RED + "FATAL ERROR: THE BALANCE COULD NOT BE LOADED!!!" + Utilities.ANSI.RESET);
+        }
+    }
+
+    public void AddMoney(Scanner sc) {
+        System.out.print(Utilities.ANSI.BOLD + "Add: " + Utilities.ANSI.RESET);
+
+        if (sc.hasNextDouble()) {
+            Value = sc.nextDouble();
+            this.Balance += Value;
+        } else {
+            System.out.println("Invalid input. Please enter a valid number.");
+            sc.next();
+        }
+    }
+
+    public void ToTransferMoney(Scanner sc) {
+        System.out.print(Utilities.ANSI.BOLD + "To transfer: ");
+
+        if (sc.hasNextDouble()) {
+            Value = sc.nextDouble();
+            this.Balance -= Value;
+        } else {
+            System.out.println("Invalid input. Please enter a valid number.");
+            sc.next();
+        }
+    }
+}
---
+++ Utilities.java
@@ -0,0 +1,10 @@
+public class Utilities {
+    public class ANSI {
+        public static final String RESET = "\u001B[0m";
+        public static final String BOLD  = "\u001B[1m";
+
+        public static final String BOLD_RED = "\u001B[1m\u001B[31m";
+        public static final String BOLD_GREEN = "\u001B[1m\u001B[32m";
+        public static final String BOLD_YELLOW = "\u001B[1m\u001B[33m";
+    }
+}
